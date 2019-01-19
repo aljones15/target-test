769a9beea51722e0b6ef389c72cdb1bae15eb660
andrewjones <aljones15@gmail.com>
2019-01-19 09:41:56 -0500

    got it

---
+++ test/unit/order.test.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { expect } from 'chai'
+import { shallow } from 'enzyme'
+import Order from '../src/components/item/order'
+
+
+describe('<Order />', function() {
+
+})
