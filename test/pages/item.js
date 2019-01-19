55286e2637ddbf80bdf03bdec84505980cbd38c6
andrewjones <aljones15@gmail.com>
2019-01-19 13:21:35 -0500

    began e2e tests

--- test/pages/item.js
+++ test/pages/item.js
@@ -1,21 +1,21 @@
 
 
 class ItemPage {
- get item() {
-   return $('[data-test="item-page"]')
- }
- get orderBox() {
-   return $('[data-test="order-box"]')
- }
- get reviewsBox() {
-   return $('[data-test="reviews-box"]')
- }
- get highlightsBox() {
-   return $('[data-test="highlights-box"]')
- }
- get carouselBox() {
-   return $('[data-test="carousel-box"]')
- }
+    get item() {
+        return $('[data-test="item-page"]')
+    }
+    get orderBox() {
+        return $('[data-test="order-box"]')
+    }
+    get reviewsBox() {
+        return $('[data-test="reviews-box"]')
+    }
+    get highlightsBox() {
+        return $('[data-test="highlights-box"]')
+    }
+    get carouselBox() {
+        return $('[data-test="carousel-box"]')
+    }
 }
 
 export default ItemPage
--- test/specs/item.spec.js
+++ test/specs/item.spec.js
@@ -1,8 +1,31 @@
 import ItemPage from '../pages/item'
+import { expect } from 'chai'
+
+const itemPage = new ItemPage()
 
 describe('Item Page', function() {
     it('should visit the main page', function() {
         browser.url('/')
     })
-
+    
+    it('should see an item', function() {
+        const vis = itemPage.item.waitForDisplayed(5000)
+        expect(vis, 'expected item to display').to.be.true
+    })
+    it('should see an orderBox', function() {
+        const vis = itemPage.orderBox.waitForDisplayed(5000)
+        expect(vis, 'expected orderBox to be display').to.be.true
+    })
+    it('should see an reviewsBox', function() {
+        const vis = itemPage.reviewsBox.waitForDisplayed(5000)
+        expect(vis, 'expected reviewsBox to display').to.be.true
+    })
+    it('should see an highlightsBox', function() {
+        const vis = itemPage.highlightsBox.waitForDisplayed(5000)
+        expect(vis, 'expected highlightsBox to display').to.be.true
+    })
+    it('should see an carouselBox', function() {
+        const vis = itemPage.carouselBox.waitForDisplayed(5000)
+        expect(vis, 'expected carouselBox to display').to.be.true
+    })
 })
