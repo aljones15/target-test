7ed03bf4c632e2d6418d049b105dee459f7569f4
andrewjones <aljones15@gmail.com>
2019-01-19 09:57:46 -0500

    mocha runs

--- test/unit/order.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { expect } from 'chai'
-import { shallow } from 'enzyme'
-import Order from '../src/components/item/order'
-
-
-describe('<Order />', function() {
-
-})
---
+++ test/unit/order.unit.js
@@ -0,0 +1,9 @@
+const React = require('react')
+const { expect } = require('chai')
+const { shallow } = require('enzyme')
+const Order = require('../../src/components/item/order')
+
+
+describe('<Order />', function() {
+
+})
